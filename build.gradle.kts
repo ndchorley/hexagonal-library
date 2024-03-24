b2eed7285c2277b8cf907f7e36e18aae85e3b77a
Nicky Chorley <1155941+ndchorley@users.noreply.github.com>
2024-03-24 07:56:57 +0000

    Fix some whitespace in build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -19,7 +19,7 @@ dependencies {
     implementation("org.http4k:http4k-testing-webdriver")
 
     testImplementation(platform("org.junit:junit-bom:5.10.2"))
-	testImplementation("org.junit.jupiter:junit-jupiter")
+    testImplementation("org.junit.jupiter:junit-jupiter")
     testImplementation("io.strikt:strikt-core:0.34.1")
 }
 
