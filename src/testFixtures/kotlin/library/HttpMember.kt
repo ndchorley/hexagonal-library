13201326169711f32f054b9ce91f30680094a362
Nicky Chorley <1155941+ndchorley@users.noreply.github.com>
2024-03-24 10:15:34 +0000

    Introduce asListOfBooks in HttpMember

--- src/testFixtures/kotlin/library/HttpMember.kt
+++ src/testFixtures/kotlin/library/HttpMember.kt
@@ -5,6 +5,7 @@ import library.domain.Book
 import org.http4k.core.HttpHandler
 import org.http4k.webdriver.Http4kWebDriver
 import org.openqa.selenium.By
+import org.openqa.selenium.WebElement
 
 interface HttpMember : Member {
     val baseUri: String
@@ -18,17 +19,19 @@ interface HttpMember : Member {
         val bookDivs =
             driver.findElements(By.className("book")) ?: return emptyList()
 
-        return bookDivs.mapNotNull { div ->
-            val title = div.findElement(By.className("title"))?.text
-            val author = div.findElement(By.className("author"))?.text
-            val copies = div.findElement(By.className("copies"))?.text?.toInt()
+        return bookDivs.asListOfBooks()
+    }
 
-            when {
-                title != null && author != null && copies != null ->
-                    (Book(title, author, copies))
+    private fun List<WebElement>.asListOfBooks() = mapNotNull { div ->
+        val title = div.findElement(By.className("title"))?.text
+        val author = div.findElement(By.className("author"))?.text
+        val copies = div.findElement(By.className("copies"))?.text?.toInt()
 
-                else -> null
-            }
+        when {
+            title != null && author != null && copies != null ->
+                (Book(title, author, copies))
+
+            else -> null
         }
     }
 }
