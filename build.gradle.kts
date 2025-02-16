10e036e3d3e3b6e7690c8bf5f23aa7a2ccb0de89
Nicky Chorley <1155941+ndchorley@users.noreply.github.com>
2025-02-16 15:35:55 +0000

    Upgrade http4k to 6.0.0.0

--- build.gradle.kts
+++ build.gradle.kts
@@ -12,7 +12,7 @@ repositories {
 }
 
 dependencies {
-    implementation(platform("org.http4k:http4k-bom:5.26.0.0"))
+    implementation(platform("org.http4k:http4k-bom:6.0.0.0"))
     implementation("org.http4k:http4k-core")
     implementation("org.http4k:http4k-template-handlebars")
     implementation("org.http4k:http4k-client-apache")
